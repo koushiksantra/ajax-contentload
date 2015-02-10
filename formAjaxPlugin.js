0c2224e0bbfe3242ec9460f3a49bf9d73931ac73
Koushik Santra <koushiksantra@outlook.com>
2015-02-10 21:00:31 +0530

    form ajax plugin .js version 2 commit

--- formAjaxPlugin.js
+++ formAjaxPlugin.js
@@ -1,52 +1,84 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 (function ($) {
-            $.fn.update = function (options) {
-                var formObj = this.find("form");
-                var formUrl = formObj.attr('action');
-                var str = formObj.serialize();
-                
+    $.fn.update = function (options) {
+        console.log(this);
+        var formObj = '';
+        var formUrl = '';
+        console.log(this.is('form'));
+        console.log(this.is('a'));
+        if (this.is('form')) {
+//            formObj = this.find("form");
+            formObj = this.closest('form');
+            formUrl = formObj.attr('action');
+            var str = formObj.serialize();
+        } else if (this.is('a')) {
+
+
+            formObj = this;
+
+//         console.log(this.currentTarget)
+//            formObj = this.target;
+//
+//         console.log(this.currentTarget)
+
+
+            formUrl = formObj.attr('href');
+//            temp=formUrl.split('#').
+//            formUrl=temp[1];
+        }
+
+
 //                 console.log(this);
-                var settings = $.extend({
-                    // These are the defaults.
-                    destination:this.selector,
-                    url: formUrl,
+        var settings = $.extend({
+            // These are the defaults.
+            destination: this.selector,
+            url: formUrl,
 //                    dataType : "json",
-                    type: "POST",
-                    cache: false,
-                    data: str,
-                    success: function (response) {
+            type: "POST",
+            cache: false,
+            data: str,
+            loadView: true,
+            success: function (response) {
 
 
 
 
-                        if ($.type(response) == 'object')
-                        {
+                if ($.type(response) == 'object')
+                {
 
-                            //It is JSON
-                                 console.log(response)
-                            console.log("It is  a json type");
-                        } else {
+                    //It is JSON
+                    console.log(response)
+                    console.log("It is  a json type");
+                } else {
 
-                           
-                            $(settings.destination).html(response);
-                            // is not a JSON type
-                            // console.log(response)
-                            console.log("It is not a json type");
-                        }
-
-                    },
-                    // Code to run if the request fails; the raw request and
-//                    // status codes are passed to the function
-                    error: function (xhr, status, errorThrown) {
-                        alert("Sorry, there was a problem!");
-                        console.log("Error: " + errorThrown);
-                        console.log("Status: " + status);
-                        console.dir(xhr);
-                    },
-                    complete: function (xhr, status) {
-                        alert("The request is complete!");
+                    if (settings.loadView) {
+                        $(settings.destination).html(response);
                     }
-                }, options);
-                $.ajax(settings)
+
+                    // is not a JSON type
+                    // console.log(response)
+                    console.log("It is not a json type");
+                }
+
+            },
+            // Code to run if the request fails; the raw request and
+//                    // status codes are passed to the function
+            error: function (xhr, status, errorThrown) {
+                alert("Sorry, there was a problem!");
+                console.log("Error: " + errorThrown);
+                console.log("Status: " + status);
+                console.dir(xhr);
+            },
+            complete: function (xhr, status) {
+               // alert("The request is complete!");
+            }
+        }, options);
+        $.ajax(settings)
 
 
 
@@ -58,5 +90,5 @@
 
 
 //                return this;
-            }
-        }(jQuery));
+    }
+}(jQuery));
